de99676678432e4da7dce6a8a8bef1d55574caea
SSGGXX <shenguoxin@vupt.edu.cn>
2019-11-05 18:30:06 +0800

    主程序入口

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ import redis
 from utils.tet_bert_keras_sim import SimTwoQuestion
 from utils.redis_db import get_data_from_db_by_name, generate_html_by_data
 
-from utils.redis_db import update_QA_db
+from utils.redis_db import update_db
 from utils.BaiduTransAPI import trans_question
-from utils.cut_td_idf import main
+from utils.cut_td_idf import cut_td_idf_main
 from utils.redis_db import update_history_question, update_common_question
 from utils.redis_db import remove_temp_pic
 
@@ -29,21 +29,18 @@ class Query:
 
     def __init__(self):
         remove_temp_pic()
-        update_QA_db()
-        remove_temp_pic()
-        main()
+        if input("是否添加新的问题？(Y/N)") == "Y":
+            update_db()
+            remove_temp_pic()
+        cut_td_idf_main()
         r = redis.Redis(host="127.0.0.1", port=6379, db=0)
         questions = [q.decode('utf-8') for q in r.keys('*')]
-        self.update_trans_questions(questions)
+        trans_question(questions)
         self.questions_dict = json.load(open('Data/txt_file/questions_dict.json', 'r', encoding="utf-8"))
         self.questions = list(self.questions_dict.keys())
         self.sim_two_question = SimTwoQuestion(self.questions)
         r.close()
         print("完成初始化Query！")
-
-    @staticmethod
-    def update_trans_questions(questions):
-        trans_question(questions)
 
     def query(self, question):
         scores = self.sim_two_question.sim_questions(question)
@@ -66,7 +63,5 @@ class Query:
 
 if __name__ == '__main__':
     query = Query()
-
     while True:
-        question = input("请输入问题：")
-        print(query.simple_query(question))
+        print(query.simple_query(input("请输入问题：")))
